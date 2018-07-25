2aa4366e9fde71ef749b26fa5cbf52cf2cec7e65
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2018-07-25 22:29:20 +0200

    grammar: for rule.lisp

--- src/grammar/rule.lisp
+++ src/grammar/rule.lisp
@@ -1,17 +1,20 @@
-;;;; rule.lisp --- .
+;;;; rule.lisp --- Rule and forward referenced rule classes.
 ;;;;
-;;;; Copyright (C) 2017 Jan Moringen
+;;;; Copyright (C) 2017, 2018 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:parser.packrat.grammar)
 
-;;;
+;;; `rule-base' class
 
-(defclass rule-base (c2mop:funcallable-standard-object
-                     named-mixin)
+(defclass rule-base (named-mixin
+                     print-items:print-items-mixin
+                     c2mop:funcallable-standard-object)
   ()
-  (:metaclass c2mop:funcallable-standard-class))
+  (:metaclass c2mop:funcallable-standard-class)
+  (:documentation
+   "Superclass for rule classes."))
 
 #+todo (defmethod rule-function ((rule rule-base))
   )
@@ -19,11 +22,13 @@
 (defmethod (setf rule-function) ((new-value function) (rule rule-base))
   (c2mop:set-funcallable-instance-function rule new-value))
 
-;;;
+;;; `forward-referenced-rule' class
 
 (defclass forward-referenced-rule (rule-base)
   ()
-  (:metaclass c2mop:funcallable-standard-class))
+  (:metaclass c2mop:funcallable-standard-class)
+  (:documentation
+   "Instances represented undefined rules created by forward references."))
 
 (defmethod initialize-instance :after ((instance forward-referenced-rule)
                                        &key)
@@ -33,12 +38,16 @@
           (error "~@<Forward referenced rule ~A invoked.~@:>"
                  instance))))
 
-;;;
+;;; `rule'
 
 (defclass rule (rule-base)
   ((expression :initarg :expression
                :reader  expression))
-  (:metaclass c2mop:funcallable-standard-class))
+  (:metaclass c2mop:funcallable-standard-class)
+  (:default-initargs
+   :expression (more-conditions:missing-required-initarg 'rule :expression))
+  (:documentation
+   "Instances represent defined rules."))
 
 (defmethod shared-initialize :after ((instance   rule)
                                      (slot-names t)
