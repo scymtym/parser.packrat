b89f527619e748a6929dc784e84d16d4f7db93b1
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2019-03-30 17:58:36 +0100

    bootstrap: Import value, <-, <<-, ? from respective home package

--- src/bootstrap/package.lisp
+++ src/bootstrap/package.lisp
@@ -9,6 +9,15 @@
   (:shadowing-import-from #:parser.packrat.expression
    #:variable)
 
+  (:shadowing-import-from #:parser.packrat.grammar.base
+   #:value)
+
+  (:import-from #:parser.packrat.grammar.base
+   #:<- #:<<-)
+
+  (:import-from #:parser.packrat.grammar.sequence
+   #:?)
+
   (:local-nicknames
    (#:base #:parser.packrat.grammar.base)
    (#:seq  #:parser.packrat.grammar.sequence))
