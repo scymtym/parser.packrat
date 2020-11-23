8e20f481d3534dcfa380a15784d203fed7ca1a9a
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2020-11-23 13:24:13 +0100

    grammar.base: More source tracking in meta-grammar

--- src/grammar/base/concrete-syntax.lisp
+++ src/grammar/base/concrete-syntax.lisp
@@ -54,15 +54,17 @@
   (list* name arguments))
 
 (parser.packrat:defrule function-name-or-partial-application! ()
-    (must (function-name-or-partial-application) "must be a function name or partial application"))
+    (must (function-name-or-partial-application)
+          "must be a function name or partial application"))
 
 (parser.packrat:defrule predicate-expression (context)
-    (list (or :guard 'guard)
-          (or (parser.packrat.grammar.sequence:seq
-               (<- sub-expression (expression! context))
-               (<- predicate (function-name-or-partial-application!)))
-              (<- predicate (function-name-or-partial-application!))))
-  (bp:node* (:predicate :predicate predicate)
+    (value (source)
+      (list (or :guard 'guard)
+            (or (parser.packrat.grammar.sequence:seq
+                 (<- sub-expression (expression! context))
+                 (<- predicate (function-name-or-partial-application!)))
+                (<- predicate (function-name-or-partial-application!)))))
+  (bp:node* (:predicate :predicate predicate :source source)
     (1 :sub-expression (or sub-expression (bp:node* (:anything))))))
 
 (parser.packrat:defrule anything-expression (context)
@@ -72,11 +74,12 @@
 ;;; Constant
 
 (parser.packrat:defrule constant-expression (context)
-    (or (list 'quote value)
-        (guard value (typep '(not (or cons (and symbol (not keyword)))))))
+    (value (source)
+      (or (list 'quote value)
+          (guard value (typep '(not (or cons (and symbol (not keyword))))))))
   (ecase context
-    (:value   (bp:node* (:constant :value value)))
-    (:default (bp:node* (:terminal :value value)))))
+    (:value   (bp:node* (:constant :value value :source source)))
+    (:default (bp:node* (:terminal :value value :source source)))))
 
 ;;; Variables
 
@@ -87,33 +90,36 @@
     (must (variable-name) "must be a variable name"))
 
 (parser.packrat:defrule implicit-list (action)
-    (list (* (<<- variables (variable-name!)) 1))
+    (value (source)
+      (list (* (<<- variables (variable-name!)) 1)))
   (let ((expressions
           (loop :for variable :in (nreverse variables)
                 :collect (bp:node* (action :variable variable)
                            (1 :sub-expression (bp:node* (:anything)))))))
-    (bp:node* (:as-list)
+    (bp:node* (:as-list :source source)
       (1 :sub-expression (bp:node* (:sequence)
                            (* :sub-expression expressions))))))
 
 (parser.packrat:defrule set-expression/simple (context)
-    (or (<- variable1 (variable-name))
-        (list (or :<- '<-)
-              (and (not (list* :any)) (<- variable2 (variable-name!)))
-              (* (<- sub-expression (expression! context)) 0 1)))
+    (value (source)
+      (or (<- variable1 (variable-name))
+          (list (or :<- '<-)
+                (and (not (list* :any)) (<- variable2 (variable-name!)))
+                (* (<- sub-expression (expression! context)) 0 1))))
   (if (and variable1 (eq context :value))
-      (bp:node* (:variable-reference :variable variable1))
-      (bp:node* (:set :variable (or variable1 variable2))
+      (bp:node* (:variable-reference :variable variable1 :source source))
+      (bp:node* (:set :variable (or variable1 variable2) :source source)
         (1 :sub-expression (or sub-expression (bp:node* (:anything)))))))
 
 (parser.packrat:defrule set-expression/compose (context)
-    (list '<-
-          (<- set (or (implicit-list :set) (expression! context)))
-          (* (<<- sub-expressions (expression! context))))
+    (value (source)
+      (list '<-
+            (<- set (or (implicit-list :set) (expression! context)))
+            (* (<<- sub-expressions (expression! context)))))
   (when (eq context :value)
     (:fail))
   (if sub-expressions
-      (bp:node* (:compose)
+      (bp:node* (:compose :source source)
         (* :sub-expression (append sub-expressions (list set))))
       set))
 
@@ -122,20 +128,22 @@
         (set-expression/compose context)))
 
 (parser.packrat:defrule push-expression/simple (context)
-    (list (or :<<- '<<-)
-          (and (not (list* :any)) (<- variable (variable-name!)))
-          (* (<- sub-expression (expression! context)) 0 1))
-  (bp:node* (:push :variable variable)
+    (value (source)
+      (list (or :<<- '<<-)
+            (and (not (list* :any)) (<- variable (variable-name!)))
+            (* (<- sub-expression (expression! context)) 0 1)))
+  (bp:node* (:push :variable variable :source source)
     (1 :sub-expression (or sub-expression (bp:node* (:anything))))))
 
 (parser.packrat:defrule push-expression/compose (context)
-    (list '<<-
-          (<- push (or (implicit-list :push) (expression! context)))
-          (* (<<- sub-expressions (expression! context))))
+    (value (source)
+      (list '<<-
+            (<- push (or (implicit-list :push) (expression! context)))
+            (* (<<- sub-expressions (expression! context)))))
   (when (eq context :value)
     (:fail))
   (if sub-expressions
-      (bp:node* (:compose)
+      (bp:node* (:compose :source source)
         (* :sub-expression (append sub-expressions (list push))))
       push))
 
@@ -146,10 +154,11 @@
 ;;; Must
 
 (parser.packrat:defrule must-expression (context)
-    (list (or :must 'must)
-          (<- sub-expression (expression! context))
-          (* (guard message stringp) 0 1))
-  (bp:node* (:must :message message)
+    (value (source)
+      (list (or :must 'must)
+            (<- sub-expression (expression! context))
+            (* (guard message stringp) 0 1)))
+  (bp:node* (:must :message message :source source)
     (1 :sub-expression sub-expression)))
 
 ;;; Logical connectives
@@ -177,10 +186,11 @@
 ;;;
 
 (parser.packrat:defrule transform-expression (context)
-    (list* :transform
-           (<- sub-expression (expression! context)) ; TODO this is not optional. make that work with :must
-           code)
-  (bp:node* (:transform :code code)
+    (value (source)
+      (list* (or :transform 'transform) ; TODO export
+             (<- sub-expression (expression! context)) ; TODO this is not optional. make that work with :must
+             code))
+  (bp:node* (:transform :code code :source source)
     (1 :sub-expression sub-expression)))
 
 ;;; Rule invocation
@@ -198,17 +208,21 @@
     (must (grammar-name) "must be a grammar name"))
 
 (parser.packrat:defrule rule-invocation-expression (context)
-    (list (or (list (<- rule-name    (rule-name!))
-                    (<- grammar-name (grammar-name!)))
-              (<- rule-name (rule-name)))
-          (* (<<- arguments (expression! :value))))
+    (value (source)
+      (list (or (list (<- rule-name    (rule-name!))
+                      (<- grammar-name (grammar-name!)))
+                (<- rule-name (rule-name)))
+            (* (<<- arguments (expression! :value)))))
   (bp:node* (:rule-invocation :grammar grammar-name
-                              :rule    rule-name)
+                              :rule    rule-name
+                              :source  source)
     (* :sub-expression (nreverse arguments))))
 
 (parser.packrat:defrule next-rule-invocation-expression (context)
-    (list (or :next-rule 'next-rule) (* (<<- arguments (expression! :value))))
-  (bp:node* (:next-rule-invocation)
+    (value (source)
+      (list (or :next-rule 'next-rule)
+            (* (<<- arguments (expression! :value)))))
+  (bp:node* (:next-rule-invocation :source source)
     (* :sub-expression (nreverse arguments))))
 
 ;;; Position
